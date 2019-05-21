b760c516f8fe3e190b65652ae37538ef62c14467
yutoj <yutoogata4@gmail.com>
2019-05-21 23:06:45 +0900

    add dotenv

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div>hi</div>
-  );
+const GITHUB_ACCESS_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
+
+class App extends Component {
+  render() {
+    return (
+      <div>hi</div>
+    )
+  }
 }
 
 export default App
