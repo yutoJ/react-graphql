56aa96f48474131db0736c56e4986a4f2137ec57
yutoj <yutoogata4@gmail.com>
2019-06-08 11:05:37 +0900

    search repositories

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React, { Component } from 'react'
 import client from './client'
 import { ApolloProvider } from 'react-apollo'
-import gql from 'graphql-tag'
 import { Query } from 'react-apollo'
+import { ME, SEARCH_REPOSITORIES } from './graphql'
 
-const ME = gql`
-  query me {
-    user(login: "yutoj2") {
-      name
-      avatarUrl
-    }
-  }
-`
+const VARIABLES = {
+  first: 10,
+  after: null,
+  last: null,
+  before: null,
+  query: "swiftui"
+}
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = VARIABLES
+  }
+
   render() {
+    const { query, first, last, before, after } = this.state
     return (
       <ApolloProvider client={client}>
         <Query query={ME}>
@@ -24,6 +29,19 @@ class App extends Component {
               if (error) return `Error! ${error.message}`
 
               return <div>{data.user.name}</div>
+            }
+          }
+        </Query>
+        <Query
+          query={SEARCH_REPOSITORIES}
+          variables={{ query, first, last, before, after }}
+        >
+          { 
+            ({ loading, error, data }) => {
+              if (loading) return 'Loading...'
+              if (error) return `Error! ${error.message}`
+              console.log({data})
+              return <div></div>
             }
           }
         </Query>
--- src/graphql.js
+++ src/graphql.js
@@ -8,3 +8,30 @@ export const ME = gql`
     }
   }
 `
+export const SEARCH_REPOSITORIES = gql`
+  query searchRepositories($first: Int, $after: String, $last: Int, $before: String, $query: String!) {
+    search(first: $first, after: $after, last: $last, before: $before, query: $query, type: REPOSITORY) {
+      repositoryCount
+      pageInfo {
+        endCursor
+        hasNextPage
+        hasPreviousPage
+        startCursor
+      }
+      edges {
+        cursor
+        node {
+          ... on Repository {
+            id
+            name
+            url
+            stargazers {
+              totalCount
+            }
+            viewerHasStarred
+          }
+        }
+      }
+    }
+  }
+`
